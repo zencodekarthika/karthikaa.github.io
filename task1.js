7c6bbfd086eef50f01c15c122bc5b99e13424bf7
zencodekarthika <45650340+zencodekarthika@users.noreply.github.com>
2018-12-20 17:33:23 +0530

    upload

--- task1.js
+++ task1.js
@@ -9,26 +9,29 @@ var employee = [
         dateofjoining: "oct 10-10-2018",
         dateofbirth:"24--06-1996",
         vehicle: "Bike",
-        experience:"5",
+        experience:"2",
+        metropolitancity:"yes"
 
     },
     { 
         name: "soundariya",
         age: "30",
-        role:"Hr",
+        role:"Project manager",
         gender: "female",
-        location: "Banglorei",
+        location: "Banglore",
         contactNo:"8754775467",
         dateofjoining: "oct 10-10-2018",
         dateofbirth:"22/06/1994",
         vehicle: "car",
-        experience:"5",
+        experience:"10",
+        metropolitancity:"yes"
+
         
     }, 
     {
         name: "Hariharan",
         age: "46",
-        role:"team manager",
+        role:"project Manager",
         gender: "male",
         location: "chennai",
         contactNo:"7567892345",
@@ -36,18 +39,22 @@ var employee = [
         dateofbirth:"17-10-1996",
         vehicle: "bike",
         experience:"10",
+        metropolitancity:"yes"
+
     },
         {
         name: "Raji",
         age: "26",
         gender: "female",
-        role:"Hr",
+        role:"Team Leader",
         location: "coimbatore",
         contactNo:"9994965827",
-        dateofjoining: " Feb 23/02/2018",
+        dateofjoining: " Feb 26/02/2018",
         dateofbirth:"oct 25-10-2018",
         vehicle: "null",
         experience:"5",
+        metropolitancity:"yes"
+
         },
 
         {
@@ -61,6 +68,8 @@ var employee = [
         dateofbirth:"12-06-1995",
         vehicle: "car",
         experience:"2",
+        metropolitancity:"yes"
+
         },
         
         {
@@ -74,30 +83,36 @@ var employee = [
         dateofbirth:"12-06-1994",
         vehicle: "car",
         experience:"2",
+        metropolitancity:"yes"
+
         },
         {
         name: "kumar",
-        age: "42",
+        age: "42", 
         gender: "male",
-        role:"Hr",
+        role:"Team Leader",
         location: "coimbatore",
         contactNo:"7789654321",
         dateofjoining: " sep 21-09-2017",
         dateofbirth:"13-06-1978",
         vehicle: "car",
         experience:"5",
+        metropolitancity:"yes"
+
         },
         {
         name: "Parthiban",
         age: "24",
         gender: "male",
-        role:"Hr",
+        role:"Developer",
         location: "coimbatore",
         contactNo:"876543219",
         dateofjoining: " sep 22-09-2018",
         dateofbirth:"13-06-1978",
         vehicle: "car",
         experience:"2",
+        metropolitancity:"yes"
+
         },
         {
         name: "viji",
@@ -109,7 +124,9 @@ var employee = [
         dateofjoining: " sep 22-09-2018",
         dateofbirth:"17-06-1976",
         vehicle: "car",
-        experience:"10",
+        experience:"2",
+        metropolitancity:"yes"
+
         },
         {
         name: "Parthiban",
@@ -121,27 +138,90 @@ var employee = [
         dateofjoining: " sep 22-09-2018",
         dateofbirth:"Dec 28-12-2012",
         vehicle: "null",
-        experience:"5",
+        experience:"2",
+        metropolitancity:"yes"
+
         },
         ];
- filterdoj();
-        function filterdoj()
-                {
-                 for(var i =0;i<employee.length;i++)
-                    {
-                       console.log(employee[i].name+ " " + employee[i].dateofjoining);
-                            
-                        }
-                       }
-    filterage();
-           
-            function filterage()
-            {
-             for(var i =0;i<employee.length;i++)
-                {
-                    if ((employee[i].age<=35)&&(employee[i].age>30)){
-                    console.log(employee[i].name+ " " + employee[i].age);
-                        
-                    }
-                }
+ filterdoj();       
+        function filterdoj(){
+            for(var i = 0;i<employee.length; i++) {
+            employee.sort(function(a, b){
+            var dateA=new Date(a.dateofjoining), dateB=new Date(b.dateofjoining);
+            return dateA-dateB;
+            })
+            console.log(employee[i].name + "  " + employee[i].dateofjoining);
+             }
             }
+filterage();                     
+        function filterage(){
+                    for(var i = 0;i<employee.length; i++) {
+                if((employee[i].age<=35)&&(employee[i].age>=30)){
+                        console.log(employee[i].name+"   "+employee[i].age);
+                        }
+         }
+             }
+filterpersons();
+                 function filterpersons(name,gender,experience){
+                        this.name = name;
+                        this.gender = gender;
+                        this.experience=experience;
+                    for(var i =0;i<employee.length;i++){
+                    console.log(employee[i].name +"  "+ employee[i].gender +"  "+ employee[i].experience+ " "+employee[i].role +"  "+ employee[i].location);
+                        }
+                        employee.sort();
+                     }
+filterexperts();
+           function filterexperts(){
+                         for(i =0;i<employee.length;i++){
+                            if(employee[i].experience==10){
+                            console.log(employee[i].name + " " + "Project Manager");
+                             }
+                            else if(employee[i].experience==5){
+                                console.log(employee[i].name + "  "+ "Team Leader");
+                            }
+                            else if(employee[i].experience==2){
+                                console.log(employee[i].name + "  "+ "Developer");
+                            }
+                            else {
+                                 console.log("sorry data not found");
+                            
+                                 }
+                                } }
+displayrole();
+            function displayrole(){
+              for(i =0;i<employee.length;i++){
+                console.log(employee[i].name + " " +employee[i].role);
+                     }
+                  }
+displaylocation();
+           function displaylocation(){
+            for(i =0;i<employee.length;i++){
+                 console.log(employee[i].name + " " +employee[i].location);
+                   }
+                 }
+displayvehicle();
+                 function displayvehicle(){
+                     for(i =0;i<employee.length;i++){
+                          console.log(employee[i].name + " " +employee[i].vehicle);
+                            }
+                        }
+displaymatchingnames();
+     function displaymatchingnames(){
+       var user= employee.filter(function(item){
+      return item.name == "Parthiban";
+
+});
+console.log(user);
+}
+    metropolitancity();
+       function metropolitancity(){
+        for(i =0;i<employee.length;i++){
+           if(employee[i].metropolitancity=="yes"){
+          console.log(employee[i].name + "  "+employee[i].metropolitancity);
+        }
+           else{
+               console.log("no data found");
+           }
+}  
+}
